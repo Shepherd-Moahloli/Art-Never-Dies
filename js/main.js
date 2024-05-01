007d4d3613f66d7f161df82ee061505dbf89afa6
Shepherd-Moahloli <shepherdmoahloli122@gmail.com>
2024-05-01 21:22:06 +0200

    we making millions

--- js/main.js
+++ js/main.js
@@ -14,20 +14,51 @@ img.onerror = function() {
 var images = ['images/takeoff1.jpeg', 'images/takeoff.jpeg', 'images/takeoff-obit-eulogy.jpg.webp', 'images/takeoffobit.jpg.webp', 'images/takeoff-1-2.jpg.webp'];
 
 var index = 0;
-
 var content = document.getElementById('content');
-
 content.style.backgroundImage = 'url(' + images[index] + ')';
 
 
 function updateImage() {
-
     index = (index + 1) % images.length;
-
-
     content.style.backgroundImage = 'url(' + images[index] + ')';
-
 }
 
 
-setInterval(updateImage, 3000);
+// var intervalId = setInterval(updateImage, 3000);    this code helps to pause animation when you hover over the image
+
+// content.addEventListener('mouseover', function() {
+//     clearInterval(intervalId);
+// });
+
+// content.addEventListener('mouseout', function() {
+//     intervalId =setInterval(updateImage, 3000);
+// })
+
+
+var image = document.querySelector('#playstore img:nth-child(2)');
+var audio = document.querySelector('#playstore audio');
+var forwardIcon = document.querySelector('#playstore img:nth-child(3)');
+var backwardIcon = document.querySelector('#playstore img:nth-child(1)');
+
+var image1 = 'images/play-solid.svg';
+var image2 = 'images/pause-solid.svg';
+
+
+image.addEventListener('click', function() {
+
+    if (image.src.endsWith(image1)) {
+        image.src = image2;
+        audio.play();
+                intervalId = setInterval(updateImage, 3000);
+    } else {
+        image.src = image1;
+        audio.pause();
+        clearInterval(intervalId)
+    }
+});
+
+
+
+
+
+
