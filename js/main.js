b612da58e9749c3cf390f2b34ff1c14019f36994
Shepherd-Moahloli <shepherdmoahloli122@gmail.com>
2024-05-01 17:16:19 +0200

    just made the animation to the image

--- js/main.js
+++ js/main.js
@@ -7,3 +7,27 @@ img.onload = function() {
 img.onerror = function() {
     console.log('Image not found');
 };
+
+
+
+
+var images = ['images/takeoff1.jpeg', 'images/takeoff.jpeg', 'images/takeoff-obit-eulogy.jpg.webp', 'images/takeoffobit.jpg.webp'];
+
+var index = 0;
+
+var content = document.getElementById('content');
+
+content.style.backgroundImage = 'url(' + images[index] + ')';
+
+
+function updateImage() {
+
+    index = (index + 1) % images.length;
+
+
+    content.style.backgroundImage = 'url(' + images[index] + ')';
+
+}
+
+
+setInterval(updateImage, 3000);
